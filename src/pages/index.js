c3a637d7adac7752e8a2fafe2fb4b6468a7c6f6e
MChiancone <Chiancone.matthew@gmail.com>
2022-03-21 10:07:48 -0400

    Basic Setup: Global and Component Styling Setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import React from "react"
 import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.css"
+import { Link } from "gatsby"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer and web developer based in Ohio.</p>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>

ee09656bdcdb52c64817d77320ab8e50def48f0b
MChiancone <Chiancone.matthew@gmail.com>
2022-03-21 12:59:23 -0400

    Basic Setup: Changed to useStaticQuery Hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
-import { Link, graphql } from "gatsby"
+import { Link } from "gatsby"
 
-export default function Home({ data }) {
-  console.log(data)
-  const { title, description } = data.site.siteMetadata
-
+export default function Home() {
   return (
     <Layout>
       <section className={styles.header}>
@@ -19,22 +16,7 @@ export default function Home({ data }) {
           </Link>
         </div>
         <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
-        <p>
-          {" "}
-          {title} - {description}
-        </p>
       </section>
     </Layout>
   )
 }
-
-export const query = graphql`
-  query SiteInfo {
-    site {
-      siteMetadata {
-        description
-        title
-      }
-    }
-  }
-`

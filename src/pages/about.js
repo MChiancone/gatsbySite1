4515698b4eb7dc78a875676cff7389c62549ad92
MChiancone <Chiancone.matthew@gmail.com>
2022-03-21 09:32:47 -0400

    Basic Setup: Pages, Navbar, Layout Component

---
+++ src/pages/about.js
@@ -0,0 +1,32 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const About = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
+          necessitatibus quisquam maiores voluptas temporibus voluptate
+          voluptatem at similique, accusantium eveniet sapiente ut facilis,
+          iusto eaque rem ab veniam harum iste?
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
+          necessitatibus quisquam maiores voluptas temporibus voluptate
+          voluptatem at similique, accusantium eveniet sapiente ut facilis,
+          iusto eaque rem ab veniam harum iste?
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
+          necessitatibus quisquam maiores voluptas temporibus voluptate
+          voluptatem at similique, accusantium eveniet sapiente ut facilis,
+          iusto eaque rem ab veniam harum iste?
+        </p>
+      </div>
+    </Layout>
+  )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <section>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer and web developer based in Ohio.</p>
+        </div>
+      </section>
+    </Layout>
+  )
 }

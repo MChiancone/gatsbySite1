17a0aa60be3982306831020d9f1427a92fb72ebd
MChiancone <Chiancone.matthew@gmail.com>
2022-03-28 09:09:29 -0400

    Setup Query Variable to generate pages based off external data and a template file

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,25 +1,46 @@
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/project-details.module.css"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const ProjectDetails = () => {
+const ProjectDetails = ({ data }) => {
+  const { html } = data.markdownRemark
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+  const image = getImage(featuredImg)
+
   return (
     <Layout>
       <div className={styles.details}>
-        <h2>title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className={styles.featured}>
-          {/* <GatsbyImage
-            image={getImage(project.frontmatter.featuredImg)}
-            alt="Image"
-          /> */}
+          <GatsbyImage image={image} alt="Image" />
         </div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+        <div
+          className={styles.html}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </div>
     </Layout>
   )
 }
 
 export default ProjectDetails
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`
